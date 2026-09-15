83e0b377a578e131f2d86ec297b0a38e1ceb78d7
hnava08 <haroldnava5000@gmail.com>
2026-09-15 08:40:44 -0400

    Add shared pages and SuperPoderes Create/Delete

---
+++ HeroesWeb/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HeroesWeb.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
